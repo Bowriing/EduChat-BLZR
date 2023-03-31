8318dd11998247444c09862ec51868377bb110a5
bowriing <E.C.BOWRING-2021@hull.ac.uk>
2023-03-31 21:25:53 +0100

    idek how im gunna do this

---
+++ EduChat/Pages/SettingsBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EduChat.Pages
+{
+    public class SettingsBase : ComponentBase
+    {
+        protected bool Enabled = false;
+        protected void DarkModeButtonClick()
+        {
+            Enabled = !Enabled;
+            Console.WriteLine(Enabled ? "enabled" : "disabled");
+        }     
+    }
+}
